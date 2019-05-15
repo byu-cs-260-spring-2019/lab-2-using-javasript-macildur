611c8b4fdc3a1975e6f698180d6670e9deb76abc
ChazComp <chaz@rag.net>
2019-05-14 22:03:15 -0600

    final

--- public/script.js
+++ public/script.js
@@ -13,19 +13,23 @@ window.onload = function()
         fetch(url)
         .then(function(response) {
         return response.json();
-        }).then(function(json) {	
+        }).then(function(json) {
+            console.log(json);	
             let results = "";
             results += '<h2>Weather in ' + json.name + "</h2>";
-            for (let i=0; i < json.weather.length; i++) {
-          results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+            for (let i=0; i < json.weather.length; i++) 
+            {
+              results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
             }
             results += '<h2>' + json.main.temp + " &deg;F</h2>"
             results += "<p>"
-            for (let i=0; i < json.weather.length; i++) {
-          results += json.weather[i].description
-          if (i !== json.weather.length - 1)
-            results += ", "
-            }
+            // for (let i=0; i < json.weather.length; i++) 
+            // {
+            //   results += json.weather[i].description
+            //   if (i !== json.weather.length - 1)
+            //     results += ", "
+            // }
+            results += json.main.humidity + "% humidity, " + "wind blowing at " + json.wind.speed + " mph"
             results += "</p>";
             document.getElementById("weatherResults").innerHTML = results;
 
@@ -35,10 +39,12 @@ window.onload = function()
             return response.json();
             }).then(function(json) {
                 let forecast = "";
-                for (let i=0; i < json.list.length; i++) {
-              forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-              forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-              forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                for (let i=0; i < json.list.length; i++) 
+                {
+                  forecast += "<div class='card'><h3>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h a') + "</h3>";
+                  forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                  forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
+                  forecast += "<p>" + json.list[i].main.humidity + "% humidity, " + "wind blowing at " + json.list[i].wind.speed + " mph" + "</p></div>"
                 }
                 document.getElementById("forecastResults").innerHTML = forecast;
             });    
